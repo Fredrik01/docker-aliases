6025fefb2fbaf0d6d13e1f23ddfc2d499dc7f2eb
Fredrik Andersson <fredrikandersson01@gmail.com>
2018-12-20 10:28:45 +0100

    Add phpunit

--- aliases.sh
+++ aliases.sh
@@ -12,5 +12,6 @@ alias yarn="docker_alias /directory node yarn"
 
 # PHP
 alias php="docker_alias /directory php:7.2-alpine php"
+alias phpunit="docker_alias /directory phpunit/phpunit"
 alias composer="docker_alias /directory composer"
 alias phpcs="docker_alias /directory herloct/phpcs"
